a5d8fd63529af77ee26b0d63564592d040dc6436
hades <ocean871209@gmail.com>
2021-12-09 17:26:03 +0800

    新建專案

---
+++ TwenGO/TwenGO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TwenGO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
